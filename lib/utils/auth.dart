3e6e0a228b48356a3f0ec0566dd3e24c95ee0080
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-09-26 14:27:09 +0600

    got api response

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 class UserData {
   final String name;
   final String email;
@@ -21,7 +20,8 @@ class UserData {
 }
 
 class AuthProvider with ChangeNotifier {
-  String? _loggedInUser; // Store the logged-in user's information (e.g., email).
+  String?
+      _loggedInUser; // Store the logged-in user's information (e.g., email).
 
   // Getter to check if a user is logged in.
   bool get isLoggedIn => _loggedInUser != null;
@@ -30,17 +30,17 @@ class AuthProvider with ChangeNotifier {
     final url = Uri.parse('http://13.213.35.25/api/v1/auth/login');
     print('Email from auth: $email');
     print('Password from auth: $password');
+    print('Before HTTP POST request');
     final response = await http.post(
       url,
-      
       body: {
         'email': email,
         'password': password,
       },
     );
-
-    
-
+    print('After HTTP POST request');
+    print('Response Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
     if (response.statusCode == 200) {
       final responseData = json.decode(response.body);
       // Check the response data for success or failure.
