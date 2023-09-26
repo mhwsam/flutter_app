0e352cbdc99733a73d0208e72bbaa7093c5a0449
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-09-26 15:48:48 +0600

    geeting data in the homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,46 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:burobd/utils/auth.dart'; // Import your authentication provider file.
+
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home'),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.logout),
+//             onPressed: () {
+//               authProvider.logout();
+//               Navigator.pushReplacementNamed(context, '/login');
+//             },
+//           ),
+//         ],
+//       ),
+//       body: const Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text('Welcome to the Home Screen!'),
+//             // ... (Other widgets you want to display)
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:burobd/utils/auth.dart'; // Import your authentication provider file.
+import 'package:burobd/utils/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final apiResponse = authProvider.apiResponse;
 
     return Scaffold(
       appBar: AppBar(
@@ -20,11 +55,23 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('Welcome to the Home Screen!'),
+
+            // Check if the API response is available
+            if (apiResponse != null)
+              Column(
+                children: [
+                  Text('API Response Data:'),
+                  // Display the API response data here as needed
+                  // Example: Display the user's name
+                  Text('User Name: ${apiResponse['user']['name']}'),
+                  // Add more widgets to display other data from the API response
+                ],
+              ),
             // ... (Other widgets you want to display)
           ],
         ),
--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -22,9 +22,11 @@ class UserData {
 class AuthProvider with ChangeNotifier {
   String?
       _loggedInUser; // Store the logged-in user's information (e.g., email).
+  Map<String, dynamic>? _apiResponse; // Store the API response data
 
   // Getter to check if a user is logged in.
   bool get isLoggedIn => _loggedInUser != null;
+  Map<String, dynamic>? get apiResponse => _apiResponse; // Getter for API response
 
   Future<bool> login(String email, String password) async {
     final url = Uri.parse('http://13.213.35.25/api/v1/auth/login');
@@ -41,12 +43,17 @@ class AuthProvider with ChangeNotifier {
     print('After HTTP POST request');
     print('Response Status Code: ${response.statusCode}');
     // print('Response Body: ${response.body}');
+    
+
+
     if (response.statusCode == 200) {
+      
       print("hiiiii");
       final responseData = json.decode(response.body);
       // Check the response data for success or failure.
       if (responseData['status'] == true) {
         _loggedInUser = email;
+        _apiResponse = responseData; // Store the API response data
         notifyListeners();
         return true;
       }
@@ -56,6 +63,7 @@ class AuthProvider with ChangeNotifier {
 
   void logout() {
     _loggedInUser = null;
+    _apiResponse = null; // Clear the API response data on logout
     notifyListeners();
   }
 }
