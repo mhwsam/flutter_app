ecaf18894f1eaa1129bd5ed64236dc4f86100990
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-10-04 17:12:27 +0600

    "Load More" is working

--- lib/screens/categories/refrigerator_page.dart
+++ lib/screens/categories/refrigerator_page.dart
@@ -72,7 +72,120 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'package:burobd/utils/api_config.dart';
+// import 'package:burobd/utils/auth.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:provider/provider.dart';
 
+// class RefrigeratorPage extends StatefulWidget {
+//   @override
+//   _RefrigeratorPageState createState() => _RefrigeratorPageState();
+// }
+
+// class _RefrigeratorPageState extends State<RefrigeratorPage> {
+//   List<String> productTitles = [];
+//   int currentPage = 1;
+//   bool isLoading = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchData();
+//   }
+
+//   Future<void> fetchData({bool loadMore = false}) async {
+//     if (isLoading) return;
+
+//     setState(() {
+//       isLoading = true;
+//     });
+
+//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
+//     final apiUrl = Uri.parse(
+//         '${ApiConfig.baseUrl}api/v1/category/refrigerator?page=$currentPage');
+
+//     try {
+//       final response = await http.get(
+//         apiUrl,
+//         headers: {
+//           'Authorization': 'Bearer ${authProvider.apiResponse?['access_token']}',
+//         },
+//       );
+
+//       if (response.statusCode == 200) {
+//         final data = jsonDecode(response.body);
+//         final productsData = data['data']['products']['data'];
+
+//         if (productsData != null) {
+//           final products = productsData as List<dynamic>;
+
+//           // Clear the productTitles list before adding the new products from page 2.
+//           if (loadMore) {
+//             productTitles.clear();
+//           }
+
+//           productTitles.addAll(products.map((product) => product['title'] as String));
+//         } else {
+//           throw Exception('No products data in API response');
+//         }
+
+//         final nextPage = data['data']['products']['next_page_url'];
+
+//         if (nextPage != null) {
+//           currentPage++;
+//         }
+//       } else {
+//         throw Exception(
+//             'Failed to fetch data. Status Code: ${response.statusCode}');
+//       }
+//     } catch (e) {
+//       print('Error: $e');
+//     } finally {
+//       setState(() {
+//         isLoading = false;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Refrigerators'),
+//       ),
+//       body: productTitles.isEmpty
+//           ? Center(child: CircularProgressIndicator())
+//           : Column(
+//               children: [
+//                 Expanded(
+//                   child: ListView.builder(
+//                     itemCount: productTitles.length,
+//                     itemBuilder: (BuildContext context, int index) {
+//                       return Card(
+//                         child: ListTile(
+//                           title: Text(productTitles[index]),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//                 if (isLoading)
+//                   Center(child: CircularProgressIndicator())
+//                 else if (currentPage <= 2) // Show "Load More" button only if there are more pages to load
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       fetchData(loadMore: true);
+//                     },
+//                     child: Text('Load More'),
+//                   ),
+//               ],
+//             ),
+
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
 import 'package:burobd/utils/api_config.dart';
@@ -112,7 +225,8 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
       final response = await http.get(
         apiUrl,
         headers: {
-          'Authorization': 'Bearer ${authProvider.apiResponse?['access_token']}',
+          'Authorization':
+              'Bearer ${authProvider.apiResponse?['access_token']}',
         },
       );
 
@@ -123,12 +237,9 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
         if (productsData != null) {
           final products = productsData as List<dynamic>;
 
-          // Clear the productTitles list before adding the new products from page 2.
-          if (loadMore) {
-            productTitles.clear();
-          }
-
-          productTitles.addAll(products.map((product) => product['title'] as String));
+          // Add the new products to the productTitles list.
+          productTitles
+              .addAll(products.map((product) => product['title'] as String));
         } else {
           throw Exception('No products data in API response');
         }
@@ -175,7 +286,8 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
                 ),
                 if (isLoading)
                   Center(child: CircularProgressIndicator())
-                else if (currentPage <= 2) // Show "Load More" button only if there are more pages to load
+                else if (currentPage <=
+                    2) // Show "Load More" button only if there are more pages to load
                   ElevatedButton(
                     onPressed: () {
                       fetchData(loadMore: true);
@@ -184,11 +296,9 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
                   ),
               ],
             ),
-            
     );
   }
 }
-
 
 // ahasan.ullah@fel.com.bd
 // 01716167744
