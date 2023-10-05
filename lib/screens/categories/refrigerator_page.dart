e95b5457222d867013870fc5b889c042b8a5a1b0
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-10-05 13:26:24 +0600

    more datas comming

--- lib/screens/categories/refrigerator_page.dart
+++ lib/screens/categories/refrigerator_page.dart
@@ -72,6 +72,120 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'package:burobd/utils/api_config.dart';
+// import 'package:burobd/utils/auth.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:provider/provider.dart';
+
+// class RefrigeratorPage extends StatefulWidget {
+//   @override
+//   _RefrigeratorPageState createState() => _RefrigeratorPageState();
+// }
+
+// class _RefrigeratorPageState extends State<RefrigeratorPage> {
+//   List<String> productTitles = [];
+//   int currentPage = 1;
+//   bool isLoading = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchData();
+//   }
+
+//   Future<void> fetchData({bool loadMore = false}) async {
+//     if (isLoading) return;
+
+//     setState(() {
+//       isLoading = true;
+//     });
+
+//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
+//     final apiUrl = Uri.parse(
+//         '${ApiConfig.baseUrl}api/v1/category/refrigerator?page=$currentPage');
+
+//     try {
+//       final response = await http.get(
+//         apiUrl,
+//         headers: {
+//           'Authorization':
+//               'Bearer ${authProvider.apiResponse?['access_token']}',
+//         },
+//       );
+
+//       if (response.statusCode == 200) {
+//         final data = jsonDecode(response.body);
+//         final productsData = data['data']['products']['data'];
+//         // final productsData = data['data']['products']['data'][0]['companies'][0]['pivot']['price'];
+
+//         if (productsData != null) {
+//           final products = productsData as List<dynamic>;
+
+//           // Add the new products to the productTitles list.
+//           productTitles
+//               .addAll(products.map((product) => product['title'] as String));
+//         } else {
+//           throw Exception('No products data in API response');
+//         }
+
+//         final nextPage = data['data']['products']['next_page_url'];
+
+//         if (nextPage != null) {
+//           currentPage++;
+//         }
+//       } else {
+//         throw Exception(
+//             'Failed to fetch data. Status Code: ${response.statusCode}');
+//       }
+//     } catch (e) {
+//       print('Error: $e');
+//     } finally {
+//       setState(() {
+//         isLoading = false;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Refrigerators'),
+//       ),
+//       body: productTitles.isEmpty
+//           ? Center(child: CircularProgressIndicator())
+//           : Column(
+//               children: [
+//                 Expanded(
+//                   child: ListView.builder(
+//                     itemCount: productTitles.length,
+//                     itemBuilder: (BuildContext context, int index) {
+//                       return Card(
+//                         child: ListTile(
+//                           title: Text(productTitles[index]),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//                 if (isLoading)
+//                   Center(child: CircularProgressIndicator())
+//                 else if (currentPage <=
+//                     2) // Show "Load More" button only if there are more pages to load
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       fetchData(loadMore: true);
+//                     },
+//                     child: Text('Load More'),
+//                   ),
+//               ],
+//             ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:burobd/utils/api_config.dart';
 import 'package:burobd/utils/auth.dart';
@@ -85,7 +199,7 @@ class RefrigeratorPage extends StatefulWidget {
 }
 
 class _RefrigeratorPageState extends State<RefrigeratorPage> {
-  List<String> productTitles = [];
+  List<Map<String, dynamic>> productsInfo = [];
   int currentPage = 1;
   bool isLoading = false;
 
@@ -119,12 +233,22 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
         final data = jsonDecode(response.body);
         final productsData = data['data']['products']['data'];
 
+// Inside the try block, after parsing productsData
         if (productsData != null) {
           final products = productsData as List<dynamic>;
 
-          // Add the new products to the productTitles list.
-          productTitles
-              .addAll(products.map((product) => product['title'] as String));
+          // Extract and store additional information for each product
+          products.forEach((product) {
+            final pivotData = product['companies'][0]['pivot'];
+            final productInfo = {
+              'title': product['title'] as String,
+              'photo': product['photo'] as String,
+              'regular_price': pivotData['price'],
+              'discount_price': pivotData['discount_price'],
+              'stock': pivotData['stock'],
+            };
+            productsInfo.add(productInfo);
+          });
         } else {
           throw Exception('No products data in API response');
         }
@@ -153,17 +277,28 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
       appBar: AppBar(
         title: Text('Refrigerators'),
       ),
-      body: productTitles.isEmpty
+      body: productsInfo.isEmpty
           ? Center(child: CircularProgressIndicator())
           : Column(
               children: [
                 Expanded(
                   child: ListView.builder(
-                    itemCount: productTitles.length,
+                    itemCount: productsInfo.length,
                     itemBuilder: (BuildContext context, int index) {
+                      final productInfo = productsInfo[index];
                       return Card(
                         child: ListTile(
-                          title: Text(productTitles[index]),
+                          title: Text(productInfo['title']),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  'Regular Price: ${productInfo['regular_price']}'),
+                              Text(
+                                  'Discount Price: ${productInfo['discount_price']}'),
+                              Text('Stock: ${productInfo['stock']}'),
+                            ],
+                          ),
                         ),
                       );
                     },
@@ -184,6 +319,12 @@ class _RefrigeratorPageState extends State<RefrigeratorPage> {
     );
   }
 }
+
+
+
+
+
+
 
 // ahasan.ullah@fel.com.bd
 // 01716167744
