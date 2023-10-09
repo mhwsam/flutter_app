36086539f0a5a51c09635bcc8a6bf9c9f8fedb79
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-10-09 16:35:02 +0600

    nice final

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -39,156 +39,6 @@
 //   }
 // }
 
-// import 'package:flutter/material.dart';
-// import 'package:burobd/utils/api_config.dart';
-// import 'package:burobd/utils/auth.dart';
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'package:provider/provider.dart';
-
-// import 'package:carousel_slider/carousel_slider.dart'; // Import the CarouselSlider package
-
-// class ProductDetailsPage extends StatefulWidget {
-//   final String slug;
-
-//   ProductDetailsPage({required this.slug});
-
-//   @override
-//   _ProductDetailsPageState createState() => _ProductDetailsPageState();
-// }
-
-// class _ProductDetailsPageState extends State<ProductDetailsPage> {
-//   Map<String, dynamic> productData = {};
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     fetchData();
-//   }
-
-//   Future<void> fetchData() async {
-//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-//     final apiUrl =
-//         Uri.parse('${ApiConfig.baseUrl}api/v1/product/${widget.slug}');
-//     print(apiUrl);
-//     try {
-//       final response = await http.get(
-//         apiUrl,
-//         headers: {
-//           'Authorization':
-//               'Bearer ${authProvider.apiResponse?['access_token']}',
-//         },
-//       );
-
-//       if (response.statusCode == 200) {
-//         final data = jsonDecode(response.body);
-//         final product = data['product'];
-
-//         setState(() {
-//           productData = product;
-//         });
-//       } else {
-//         throw Exception(
-//             'Failed to fetch product details. Status Code: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       print('Error: $e');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final List<String> imageUrls = productData['galleries'] != null
-//         ? List<String>.from(
-//             productData['galleries'].map((gallery) => gallery['image']))
-//         : [];
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Product Details'),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             CarouselSlider(
-//               items: imageUrls.map((imageUrl) {
-//                 return Image.network(imageUrl, fit: BoxFit.cover);
-//               }).toList(),
-//               options: CarouselOptions(
-//                 height: 200,
-//                 viewportFraction: 1.0,
-//                 enlargeCenterPage: false,
-//                 autoPlay: true,
-//                 aspectRatio: 16 / 9,
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   Text(
-//                     productData['title'] ?? '',
-//                     style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       // Implement your "Buy Now" functionality here
-//                     },
-//                     child: const Text('Buy Now'),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                       'Price: ${productData['companies'][0]['pivot']['price'] ?? ''}'),
-//                   Text(
-//                       'Discount Price: ${productData['companies'][0]['pivot']['discount_price'] ?? ''}'),
-//                   Text(
-//                       'Stock: ${productData['companies'][0]['pivot']['stock'] ?? ''}'),
-//                 ],
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Table(
-//                 border: TableBorder.all(),
-//                 children: [
-//                   const TableRow(
-//                     children: [
-//                       TableCell(
-//                         child: Text('Specifications',
-//                             style: TextStyle(fontWeight: FontWeight.bold)),
-//                       ),
-//                     ],
-//                   ),
-//                   TableRow(
-//                     children: [
-//                       TableCell(
-//                         child: Padding(
-//                           padding: const EdgeInsets.all(8.0),
-//                           child: Text(productData['specifications'] ?? ''),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 
 
 import 'package:flutter/material.dart';
@@ -309,16 +159,12 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                   const TableRow(
                     children: [
                       TableCell(
-                        child: Center(
-                          child: Text('Specifications',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                        ),
+                        child: Text('Specifications',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                       ),
                       TableCell(
-                        child: Center(
-                          child: Text('Value',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                        ),
+                        child: Text('Value',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                       ),
                     ],
                   ),
