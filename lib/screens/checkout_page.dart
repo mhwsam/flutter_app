8e0e17ec3809b030b470cf318942b6c371948551
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-10-14 17:25:56 +0600

    Working Fine

--- lib/screens/checkout_page.dart
+++ lib/screens/checkout_page.dart
@@ -1,261 +1,3 @@
-//  import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:burobd/utils/ProductProvider.dart';
-// import 'package:burobd/utils/api_config.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:burobd/utils/auth.dart';
-
-// class CheckoutPage extends StatelessWidget {
-
-//   Future<void> _placeOrder(BuildContext context, Map<String, dynamic> orderData) async {
-//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-//     final apiUrl = Uri.parse('${ApiConfig.baseUrl}api/v1/place-order');
-//     print(apiUrl);
-
-//     try {
-//       final response = await http.post(
-//         apiUrl,
-//         headers: {
-//           'Authorization':
-//               'Bearer ${authProvider.apiResponse?['access_token']}',
-//           'Content-Type': 'application/json', // Set the content type
-//         },
-//         body: jsonEncode(orderData), // Convert the orderData to JSON
-//       );
-
-//       if (response.statusCode == 200) {
-//         // Order placed successfully, you can handle the response here
-//         final responseData = jsonDecode(response.body);
-//         print('Order Response: $responseData');
-//       } else {
-//         throw Exception(
-//             'Failed to place the order. Status Code: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       print('Error: $e');
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Access the ProductProvider to retrieve the saved product data
-//     final productProvider = Provider.of<ProductProvider>(context);
-//     final productData = productProvider.productData;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Checkout'),
-//       ),
-//       body: productData != null
-//           ? Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text('Product Title: ${productData['title']}'),
-//                 Text('Product ID: ${productData['id']}'),
-//                 Text('Product SKU: ${productData['sku']}'),
-//                 Text('Product UUID: ${productData['uuid']}'),
-//                 Text('Photo: ${productData['photo']}'),
-//                 Text(
-//                     'Price: ${productData['companies'][0]['pivot']['price'] ?? ''}'),
-//                 Text(
-//                     'Discount Price: ${productData['companies'][0]['pivot']['discount_price'] ?? ''}'),
-//                 Text(
-//                     'Stock: ${productData['companies'][0]['pivot']['stock'] ?? ''}'),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     // Preparing the order data here
-//                     final orderData = {
-//                       'shipping_name': "Ahasan Ullah",
-//                       'customer_id': "afsda101",
-//                       'nid': 123456,
-//                       'id': productData['id'],
-//                       'photo': productData['photo'],
-//                       'cartQuantity': 1,
-//                       'price':
-//                           productData['companies'][0]['pivot']['price'] ?? 0,
-//                       'discount_price': productData['companies'][0]['pivot']
-//                               ['discount_price'] ??
-//                           0,
-//                       'sku': productData['sku'],
-//                       'product_tpe': 'CE',
-//                       'title': productData['title'],
-//                       'uuid': productData['uuid'],
-//                       'stock':
-//                           productData['companies'][0]['pivot']['stock'] ?? 0,
-//                       'vattributes': '',
-//                     };
-
-//                     // Call the _placeOrder function to make the POST request
-//                     _placeOrder(context, orderData);
-//                   },
-//                   child: Text('Place Order'),
-//                 ),
-//               ],
-//             )
-//           : const Center(
-//               child: Text('Product data is not available.'),
-//             ),
-//     );
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:burobd/utils/ProductProvider.dart';
-// import 'package:burobd/utils/api_config.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:burobd/utils/auth.dart';
-
-// class CheckoutPage extends StatelessWidget {
-//   Future<void> _placeOrder(
-//       BuildContext context, Map<String, dynamic> orderData) async {
-//     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-//     final apiUrl = Uri.parse('${ApiConfig.baseUrl}api/v1/place-order');
-//     print(apiUrl);
-
-//     try {
-//       final response = await http.post(
-//         apiUrl,
-//         headers: {
-//           'Authorization':
-//               'Bearer ${authProvider.apiResponse?['access_token']}',
-//           'Content-Type': 'application/json', // Set the content type
-//         },
-//         body: jsonEncode(orderData), // Convert the orderData to JSON
-//       );
-
-//       if (response.statusCode == 200) {
-//         // Order placed successfully, you can handle the response here
-//         final responseData = jsonDecode(response.body);
-//         print('Order Response: $responseData');
-//          _showSuccessDialog(context);
-//       } else {
-//         throw Exception(
-//             'Failed to place the order. Status Code: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       print('Error: $e');
-//     }
-//   }
-
-// void _showSuccessDialog(BuildContext context) {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: const Text('Order Placed Successfully'),
-//           content: const Text('Your order has been successfully placed.'),
-//           actions: [
-//             TextButton(
-//               child: Text('OK'),
-//               onPressed: () {
-//                 Navigator.of(context).popUntil(ModalRoute.withName('/home'));
-//               },
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Access the ProductProvider to retrieve the saved product data
-//     final productProvider = Provider.of<ProductProvider>(context);
-//     final productData = productProvider.productData;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Checkout'),
-//       ),
-//       body: productData != null
-//           ? Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text('Product Title: ${productData['title']}'),
-//                 Text('Product ID: ${productData['id']}'),
-//                 Text('Product SKU: ${productData['sku']}'),
-//                 Text('Product UUID: ${productData['uuid']}'),
-//                 Text('Photo: ${productData['photo']}'),
-//                 Text(
-//                     'Price: ${productData['companies'][0]['pivot']['price'] ?? ''}'),
-//                 Text(
-//                     'Discount Price: ${productData['companies'][0]['pivot']['discount_price'] ?? ''}'),
-//                 Text(
-//                     'Stock: ${productData['companies'][0]['pivot']['stock'] ?? ''}'),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     // Preparing the order data here
-//                     // final orderData = {
-//                     //   'shipping_name': "Ahasan Ullah",
-//                     //   'customer_id': "afsda101",
-//                     //   'nid': 123456,
-//                     //   'id': productData['id'],
-//                     //   'photo': productData['photo'],
-//                     //   'cartQuantity': 1,
-//                     //   'price':
-//                     //       productData['companies'][0]['pivot']['price'] ?? 0,
-//                     //   'discount_price': productData['companies'][0]['pivot']
-//                     //           ['discount_price'] ??
-//                     //       0,
-//                     //   'sku': productData['sku'],
-//                     //   'product_tpe': 'CE',
-//                     //   'title': productData['title'],
-//                     //   'uuid': productData['uuid'],
-//                     //   'stock':
-//                     //       productData['companies'][0]['pivot']['stock'] ?? 0,
-//                     //   'vattributes': '',
-//                     // };
-//                     final orderData = {
-//                       "cartTotalAmount": 16738,
-//                       "cartTotalQuantity": 1,
-//                       "customer_name": "Ahasan Ullah",
-//                       "shipping_name": "Ahasan Ullah",
-//                       "customer_email": "ahasan.ullah@fel.com.bd",
-//                       "shipping_email": "ahsanullah716@gmail.com",
-//                       "customer_phone": "0178922912",
-//                       "customer_address": "Mohammadpur",
-//                       "customer_city": "Dhaka",
-//                       "customer_thana": "Mohammadpur",
-//                       "payment_type": "COD",
-//                       "order_note": "Test",
-//                       "customer_id": "afsda101",
-//                       "cartType": "DEVICE",
-//                       "center_id": 15,
-//                       "nid": 123456,
-//                       "cartItems": [
-//                         {
-//                           "id": 275,
-//                           "photo": "FTL-zkTUaASUrT.webp",
-//                           "cartQuantity": 1,
-//                           "price": 23399,
-//                           "discount_price": 20338,
-//                           "sku": "SM-E135FIDDBKD",
-//                           "product_tpe": "CE",
-//                           "title": "galaxy-f13-464-gb-orange-copper",
-//                           "uuid": "58bd7bef-3e93-4a3a-83c3-dfc945fd5bd4",
-//                           "stock": 47,
-//                           "vattributes": ""
-//                         }
-//                       ]
-//                     };
-
-//                     // Call the _placeOrder function to make the POST request
-//                     _placeOrder(context, orderData);
-//                   },
-//                   child: Text('Place Order'),
-//                 ),
-//               ],
-//             )
-//           : const Center(
-//               child: Text('Product data is not available.'),
-//             ),
-//     );
-//   }
-// }
-
 // import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 // import 'package:burobd/utils/ProductProvider.dart';
@@ -679,13 +421,24 @@ class CheckoutPage extends StatefulWidget {
 
 class _CheckoutPageState extends State<CheckoutPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String? selectedCenterId;
+  @override
+  void initState() {
+    super.initState();
+    // Fetch center data when the widget is initialized
+    fetchCenterList(context).then((map) {
+      setState(() {
+        centerMap = map;
+      });
+    });
+  }
+
+  int? selectedCenterId;
   final TextEditingController shippingNameController = TextEditingController();
   final TextEditingController customerAddressController =
       TextEditingController();
   final TextEditingController customerIdController = TextEditingController();
   final TextEditingController nidController = TextEditingController();
-
+  Map<int, String> centerMap = {}; // Variable to store center data
   Future<void> _placeOrder(
       BuildContext context, Map<String, dynamic> orderData) async {
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
@@ -726,6 +479,35 @@ class _CheckoutPageState extends State<CheckoutPage> {
     }
   }
 
+  Future<Map<int, String>> fetchCenterList(BuildContext context) async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    final apiUrl = Uri.parse('${ApiConfig.baseUrl}api/v1/centers');
+    final response = await http.get(
+      apiUrl,
+      headers: {
+        'Authorization': 'Bearer ${authProvider.apiResponse?['access_token']}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List<dynamic> centerData = data['centerDetails']['data'];
+      final Map<int, String> centerMap = {};
+
+      centerData.forEach((center) {
+        final int id = center['id'];
+        final String name = center['center_name'];
+        centerMap[id] = name;
+      });
+
+      return centerMap;
+    } else {
+      throw Exception(
+          'Failed to fetch center list. Status Code: ${response.statusCode}');
+    }
+  }
+
   void _showSuccessDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -750,6 +532,8 @@ class _CheckoutPageState extends State<CheckoutPage> {
   Widget build(BuildContext context) {
     final productProvider = Provider.of<ProductProvider>(context);
     final productData = productProvider.productData;
+    String? selectedCenterName;
+    int? selectedCenterId;
 
     return Scaffold(
       appBar: AppBar(
@@ -804,18 +588,33 @@ class _CheckoutPageState extends State<CheckoutPage> {
                       return null;
                     },
                   ),
-                  DropdownButtonFormField<String>(
+                  // DropdownButtonFormField<String>(
+                  //   value: selectedCenterId,
+                  //   items: const [
+                  //     DropdownMenuItem(
+                  //       value: "2001-IT Office",
+                  //       child: Text("2001-IT Office"),
+                  //     ),
+                  //     DropdownMenuItem(
+                  //       value: "1008-Gulshan",
+                  //       child: Text("1008-Gulshan"),
+                  //     ),
+                  //   ],
+                  //   onChanged: (value) {
+                  //     setState(() {
+                  //       selectedCenterId = value;
+                  //     });
+                  //   },
+                  //   hint: const Text('Select Center'),
+                  // ),
+                  DropdownButtonFormField<int>(
                     value: selectedCenterId,
-                    items: const [
-                      DropdownMenuItem(
-                        value: "2001-IT Office",
-                        child: Text("2001-IT Office"),
-                      ),
-                      DropdownMenuItem(
-                        value: "1008-Gulshan",
-                        child: Text("1008-Gulshan"),
-                      ),
-                    ],
+                    items: centerMap.keys.map((id) {
+                      return DropdownMenuItem<int>(
+                        value: id,
+                        child: Text(centerMap[id]!),
+                      );
+                    }).toList(),
                     onChanged: (value) {
                       setState(() {
                         selectedCenterId = value;
@@ -823,6 +622,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                     },
                     hint: const Text('Select Center'),
                   ),
+
                   TextFormField(
                     controller: nidController,
                     decoration: const InputDecoration(labelText: 'NID'),
@@ -856,7 +656,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                           "customer_id": customerIdController.text,
                           "cartType": "DEVICE",
                           // "center_id": selectedCenterId,
-                          "center_id": 15,
+                          "center_id":  selectedCenterId,
                           "nid": int.parse(nidController.text),
                           "cartItems": [
                             {
@@ -866,10 +666,12 @@ class _CheckoutPageState extends State<CheckoutPage> {
                               "photo": productData['photo'],
                               "cartQuantity": 1,
                               // "price": 23399,
-                              "price": productData['companies'][0]['pivot']['price'],
+                              "price": productData['companies'][0]['pivot']
+                                  ['price'],
                               // "discount_price": 20338,
-                              "discount_price": productData['companies'][0]['pivot']['discount_price'],
-                                  
+                              "discount_price": productData['companies'][0]
+                                  ['pivot']['discount_price'],
+
                               //"sku": "SM-E135FIDDBKD",
                               "sku": productData['sku'],
                               "product_tpe": "DEVICE",
@@ -877,8 +679,9 @@ class _CheckoutPageState extends State<CheckoutPage> {
                               "title": productData['title'],
                               // "uuid": "58bd7bef-3e93-4a3a-83c3-dfc945fd5bd4",
                               "uuid": productData['uuid'],
-                              "stock": productData['companies'][0]['pivot']['stock'],
-                                  
+                              "stock": productData['companies'][0]['pivot']
+                                  ['stock'],
+
                               "vattributes": "",
                             }
                           ],
